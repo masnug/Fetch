026215a94324432405a1ed32f2397cf870dcb4d8
Andrey Kolchenko <komexx@gmail.com>
2013-03-13 18:20:52 +0400

    New method Server::getFlags() Added a simple construct test.

--- tests/Fetch/Test/MessageTest.php
+++ tests/Fetch/Test/MessageTest.php
@@ -1,9 +1,9 @@
 <?php
 
-/*
+/**
  * This file is part of the Fetch library.
  *
- * (c) Robert Hafner <tedivm@tedivm.com>
+ * (c) Andrey Kolchenko <andrey@kolchenko.me>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,11 +12,21 @@
 namespace Fetch\Test;
 
 
+use Fetch\Server;
+
 /**
- * @package Fetch
- * @author  Robert Hafner <tedivm@tedivm.com>
+ * Class MessageTest
+ *
+ * @package Fetch\Test
+ * @authod Andrey Kolchenko <andrey@kolchenko.me>
  */
 class MessageTest extends \PHPUnit_Framework_TestCase
 {
-
+    public function testConstruct()
+    {
+        $server = new Server('imap.example.com');
+        $this->assertInstanceOf('\\Fetch\\Imap', $server->getImap(), 'Default Imap wrapper must be set in constructor');
+        $this->assertEquals('imap.example.com', $server->getServerPath());
+        $this->assertContains('novalidate-cert', $server->getFlags());
+    }
 }
